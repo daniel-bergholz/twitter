0674aefc72746ca27be5d529d63d2856c7287ec3
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-14 19:45:49 -0300

    feat: login page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,29 @@
+import {
+  BackgroundContainer,
+  Button,
+  Container,
+  RightContainer,
+} from './styles'
+import Background from '../../assets/images/login-bg.png'
+import { FaTwitter } from 'react-icons/fa'
+
 const Login = () => {
-  return <h1>Página de login</h1>
+  return (
+    <Container>
+      <BackgroundContainer>
+        <img src={Background} alt="Fundo do Twitter" />
+      </BackgroundContainer>
+      <RightContainer>
+        <FaTwitter color="#d9d9d9" size={40} />
+        <h1>Acontecendo agora</h1>
+        <h2>Inscreva-se no Twitter hoje mesmo.</h2>
+        <Button>Inscreva-se com email</Button>
+        <p>
+          Já tem uma conta? <span>Entre</span>
+        </p>
+      </RightContainer>
+    </Container>
+  )
 }
 
 export default Login
