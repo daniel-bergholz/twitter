bcf42ecf0bb5aff69ee21f50fe4f02270826c59b
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-18 16:07:45 -0300

    feat: create onClose function for modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,19 +3,21 @@ import { Background, Center } from './styles'
 interface IProps {
   isOpen: boolean
   setIsOpen: (isOpen: boolean) => void
+  onClose?: () => void
 }
 
-const Modal: React.FC<IProps> = ({ children, isOpen, setIsOpen }) => {
-  const onClose = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+const Modal: React.FC<IProps> = ({ children, isOpen, setIsOpen, onClose }) => {
+  const handleClose = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     if (event.target === event.currentTarget) {
       setIsOpen(false)
+      onClose!()
     }
   }
 
   return (
     <>
       {isOpen && (
-        <Background onClick={onClose}>
+        <Background onClick={handleClose}>
           <Center>{children}</Center>
         </Background>
       )}
