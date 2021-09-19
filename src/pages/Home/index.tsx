1839963c2c56b84cd9cae9b8f6e4d9cbd2ec94a2
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-19 13:50:43 -0300

    feat: axios interceptor (apiWithAuth) for jwt token

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,10 @@
 import { useGlobalState } from '../../context/GlobalContext'
+import { apiWithAuth } from '../../services/api'
 
 function App() {
   const { auth } = useGlobalState()
+
+  apiWithAuth.get('/feed')
 
   return <h1>Usuário logado: {auth?.user.name}</h1>
 }
