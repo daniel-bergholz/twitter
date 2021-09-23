6d4f8e7c430f01b168b082a4e1272e9fcb64feb7
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-23 20:20:17 -0300

    feat(SearchInput): styles for focus state

--- src/components/SearchInput/styles.ts
+++ src/components/SearchInput/styles.ts
@@ -1,14 +1,24 @@
 import styled from 'styled-components'
 
-export const InputContainer = styled.div`
+interface IProps {
+  isOnFocus: boolean
+}
+
+export const InputContainer = styled.div<IProps>`
   width: 348px;
   padding: 12px;
   background: #202327;
-  border: 1px solid #253341;
+  border: 1px solid
+    ${(props) => (props.isOnFocus ? props.theme.color.blue : '#253341')};
   box-sizing: border-box;
   border-radius: 40px;
   display: flex;
   align-items: center;
+
+  svg {
+    stroke: ${(props) =>
+      props.isOnFocus ? props.theme.color.blue : '#8899A6'};
+  }
 `
 
 export const Input = styled.input`
@@ -19,6 +29,7 @@ export const Input = styled.input`
   font-size: 15px;
   line-height: 18px;
   margin-left: 12px;
+  width: 100%;
 
   &::placeholder {
     color: ${(props) => props.theme.color.gray};
