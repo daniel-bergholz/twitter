1daf0356fda1bc3f58c19a7312b105ce502668ee
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-24 19:14:41 -0300

    feat(Home): add main components to home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,59 @@
+import Main from '../../components/Main'
 import SearchInput from '../../components/SearchInput'
+import SideBar from '../../components/SideBar'
+import { Container } from './styles'
 
 function Home() {
-  return <SearchInput />
+  return (
+    <Container>
+      <SideBar />
+      <Main
+        fixedContent={
+          <h1 style={{ paddingTop: '15px', paddingBottom: '15px' }}>
+            Daniel Berg
+          </h1>
+        }
+      >
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+        <h1>Meu primeiro Tweet!</h1>
+        <h1>Meu segundo Tweet!</h1>
+        <h1>Meu terceiro Tweet!</h1>
+        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      </Main>
+      <SearchInput />
+    </Container>
+  )
 }
 
 export default Home
