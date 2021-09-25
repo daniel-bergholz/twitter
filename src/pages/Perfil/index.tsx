b748021a5aba3696f50d7e1a6fa296913e2da459
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-25 12:10:14 -0300

    feat(Perfil): create fixed content

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,7 +1,26 @@
-import SideBar from '../../components/SideBar'
+import { BiArrowBack } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+import PageWrapper from '../../components/PageWrapper'
+import { FixedContentContainer, FixedContentTexts } from './styles'
 
 function Perfil() {
-  return <SideBar />
+  return (
+    <PageWrapper
+      fixedContent={
+        <FixedContentContainer>
+          <Link to="/">
+            <BiArrowBack size="15px" />
+          </Link>
+          <FixedContentTexts>
+            <h1>Daniel Berg</h1>
+            <h2>11 Tweets</h2>
+          </FixedContentTexts>
+        </FixedContentContainer>
+      }
+    >
+      página de perfil
+    </PageWrapper>
+  )
 }
 
 export default Perfil
