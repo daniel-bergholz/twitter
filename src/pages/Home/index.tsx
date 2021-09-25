b22a92ff155a42d9ac5dafa0a35fdc5b6c29ec84
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-25 07:04:22 -0300

    feat: create PageWrapper component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,58 +1,51 @@
-import Main from '../../components/Main'
-import SearchInput from '../../components/SearchInput'
-import SideBar from '../../components/SideBar'
-import { Container } from './styles'
+import PageWrapper from '../../components/PageWrapper'
 
 function Home() {
   return (
-    <Container>
-      <SideBar />
-      <Main
-        fixedContent={
-          <h1 style={{ paddingTop: '15px', paddingBottom: '15px' }}>
-            Daniel Berg
-          </h1>
-        }
-      >
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-        <h1>Meu primeiro Tweet!</h1>
-        <h1>Meu segundo Tweet!</h1>
-        <h1>Meu terceiro Tweet!</h1>
-        <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
-      </Main>
-      <SearchInput />
-    </Container>
+    <PageWrapper
+      fixedContent={
+        <h1 style={{ paddingTop: '15px', paddingBottom: '15px' }}>
+          Daniel Berg
+        </h1>
+      }
+    >
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+      <h1>Meu primeiro Tweet!</h1>
+      <h1>Meu segundo Tweet!</h1>
+      <h1>Meu terceiro Tweet!</h1>
+      <div style={{ paddingTop: '50px', paddingBottom: '50px' }} />
+    </PageWrapper>
   )
 }
 
