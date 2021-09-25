36e4e74ab58978624609e057513756551a3a3b21
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-25 16:55:29 -0300

    feat(EditProfileModal): create EditProfileModal

--- src/components/EditProfileModal/index.tsx
+++ src/components/EditProfileModal/index.tsx
@@ -1,4 +1,13 @@
+import { CgClose } from 'react-icons/cg'
+import Button from '../Button'
+import Input from '../Input'
 import Modal from '../Modal'
+import {
+  ModalBody,
+  ModalHeader,
+  ModalHeaderClose,
+  ModalHeaderTitle,
+} from './styles'
 
 interface IProps {
   isOpen: boolean
@@ -8,7 +17,28 @@ interface IProps {
 const EditProfileModal: React.FC<IProps> = ({ isOpen, setIsOpen }) => {
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <h1>Oiiiii</h1>
+      <ModalHeader>
+        <ModalHeaderClose>
+          <CgClose
+            size="18px"
+            color="#D9D9D9"
+            onClick={() => setIsOpen(false)}
+          />
+          <ModalHeaderTitle>Editar perfil</ModalHeaderTitle>
+        </ModalHeaderClose>
+
+        <Button variant="white" height="33px">
+          Salvar
+        </Button>
+      </ModalHeader>
+
+      <ModalBody>
+        <Input placeholder="Nome" />
+
+        <Input placeholder="Bio" />
+
+        <Input placeholder="Senha" type="password" />
+      </ModalBody>
     </Modal>
   )
 }
