520373443f39b2018e3eb1995d85ba972abe4adb
Daniel Bergholz <bergholz.daniel@gmail.com>
2021-09-25 15:52:01 -0300

    feat(Perfil): integrate with backend route /profile

--- src/pages/Perfil/index.tsx
+++ src/pages/Perfil/index.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from 'react'
 import { BiArrowBack } from 'react-icons/bi'
 import { FaRegCalendarAlt } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import Button from '../../components/Button'
 import PageWrapper from '../../components/PageWrapper'
+import { apiWithAuth } from '../../services/api'
 import {
   Bio,
   CreatedAt,
@@ -17,7 +20,42 @@ import {
   Username,
 } from './styles'
 
+interface ITweet {
+  id: string
+  content: string
+}
+
+interface IProfile {
+  id: string
+  name: string
+  bio: string | null
+  username: string
+  email: string
+  created_at: string
+  number_of_followers: number
+  number_of_follows: number
+  tweets: ITweet[]
+}
+
 function Perfil() {
+  const [profile, setProfile] = useState<IProfile>()
+
+  const getProfile = async () => {
+    try {
+      const { data } = await apiWithAuth.get('/profile')
+      setProfile(data)
+    } catch (error) {
+      console.log({ error })
+      toast.error(
+        error?.response?.data?.message.join(', ') || 'Erro ao carregar o perfil'
+      )
+    }
+  }
+
+  useEffect(() => {
+    getProfile()
+  }, [])
+
   return (
     <PageWrapper
       fixedContent={
@@ -26,41 +64,51 @@ function Perfil() {
             <BiArrowBack size="15px" />
           </Link>
           <FixedContentTexts>
-            <h1>Daniel Berg</h1>
-            <h2>11 Tweets</h2>
+            <h1>{profile?.name}</h1>
+            <h2>{profile?.tweets.length} Tweets</h2>
           </FixedContentTexts>
         </FixedContentContainer>
       }
     >
-      <ImageContainer>
-        <img src={`https://robohash.org/${2}`} alt={'2'} />
-        <Button variant="black" height="33px">
-          Editar perfil
-        </Button>
-      </ImageContainer>
+      {profile && (
+        <>
+          <ImageContainer>
+            <img
+              src={`https://robohash.org/${profile?.username}`}
+              alt={profile?.name}
+            />
+            <Button variant="black" height="33px">
+              Editar perfil
+            </Button>
+          </ImageContainer>
 
-      <TextsContainer>
-        <Name>Daniel Berg</Name>
-        <Username>@daniel_berg</Username>
-        <Bio>
-          CTO | Maratonas Academy Desenvolvedor Fullstack Vegano UnB -
-          Engenharia de Redes
-        </Bio>
+          <TextsContainer>
+            <Name>{profile?.name}</Name>
+            <Username>@{profile?.username}</Username>
+            <Bio>{profile?.bio}</Bio>
 
-        <CreatedAtContainer>
-          <FaRegCalendarAlt color="#6d777c" size="18px" />
-          <CreatedAt>Ingressou em outubro de 2013</CreatedAt>
-        </CreatedAtContainer>
+            <CreatedAtContainer>
+              <FaRegCalendarAlt color="#6d777c" size="18px" />
+              <CreatedAt>
+                Ingressou em{' '}
+                {new Date(profile.created_at).toLocaleDateString('pt-BR', {
+                  month: 'long',
+                })}{' '}
+                de {new Date(profile.created_at).getFullYear()}
+              </CreatedAt>
+            </CreatedAtContainer>
 
-        <FollowContainer>
-          <Follow>
-            500 <span>Seguindo</span>{' '}
-          </Follow>
-          <Follow>
-            3 <span>Seguidores</span>{' '}
-          </Follow>
-        </FollowContainer>
-      </TextsContainer>
+            <FollowContainer>
+              <Follow>
+                {profile?.number_of_follows} <span>Seguindo</span>{' '}
+              </Follow>
+              <Follow>
+                {profile?.number_of_followers} <span>Seguidores</span>{' '}
+              </Follow>
+            </FollowContainer>
+          </TextsContainer>
+        </>
+      )}
     </PageWrapper>
   )
 }
